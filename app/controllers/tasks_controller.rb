9153fa2cefdfc3d1a5e8d1b3712006330f6831f6
zyunzou.yagi <zyunzou.yagi@gmail.com>
2020-07-11 12:47:31 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,27 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     @tasks = Task.all
-  end
-  
-  def show
-    @task = Task.find(params[:id])
   end
   
   def new
     @task = Task.new
   end
   
+  def show
+    @task = Task.find(params[:id])
+  end
+  
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = "Task が正常に投稿されました"
+      flash[:success] = 'タスクを追加しました。'
       redirect_to "/"
     else
-      flash.now[:danger] = "Task が投稿されませんでした"
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの追加に失敗しました。'
       render :new
     end
   end
@@ -31,25 +34,32 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     
     if @task.update(task_params)
-      flash[:success] = "Task は正常に更新されました"
+      flash[:success] = "タスクは正常に更新されました"
       redirect_to @task
     else
-      flash.now[:danger] = "Task は更新されませんでした"
+      flash.now[:danger] = "タスクは更新されませんでした"
       render :edit
     end
   end
-  
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
     
-    flash[:success] = "Task は正常に削除されました"
+    flash[:success] = "タスクは正常に削除されました"
     redirect_to "/"
   end
-  
+
   private
-  
+
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
